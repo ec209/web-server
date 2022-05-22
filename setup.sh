642b2d01b52f810ca0c1dee8ae44b7f240b9d25d
TaegiKim <taegi@vmfarms.com>
2022-05-22 16:49:45 -0400

    add shell script to setup the dependecies and run terraform at one go

---
+++ setup.sh
@@ -0,0 +1,27 @@
+# Install aws cli and check the version of it
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+aws --version
+
+# Remove the aws cli package after the setup
+rm AWSCLIV2.pkg
+
+# Configure AWS profile
+aws configure
+
+# Move to terraform folder 
+cd terraform
+
+# Setup Terraform 1.2.0 
+wget https://releases.hashicorp.com/terraform/1.2.0/terraform_1.2.0_darwin_amd64.zip
+unzip terraform_1.2.0_darwin_amd64.zip
+# Set the name as tf_12 to not override the terraform already setup
+mv terraform /usr/local/bin/tf_12
+tf_12 version
+
+# Remove terraform binary zip
+rm terraform_1.2.0_darwin_amd64.zip
+
+# Init the terraform script and apply
+tf_1.2 init
+tf_1.2 $1
